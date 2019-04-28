866c30345dba5d1fee184382c195de7ae9622726
David Stübing <44908772+dangerdave1@users.noreply.github.com>
2019-04-28 14:50:31 +0200

    Just a test commit

--- venv/Figures.py
+++ venv/Figures.py
@@ -23,6 +23,8 @@ class Figure(ABC):
     def __repr__(self):
         return str((self._x, self._y)) + ", " + self._color
 
+# I'm going to add a king class here
+
 
 class Farmer(Figure):
     def __init__(self, x, y, color):
