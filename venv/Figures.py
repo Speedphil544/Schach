dfffc7156729a07bbe4200000476f79de75b04bc
Speedphil544 <philipp.philipp.philipp@gmail.com>
2019-05-01 20:07:33 +0200

    Farmer is finished

--- venv/Figures.py
+++ venv/Figures.py
@@ -6,7 +6,7 @@ class Figure(ABC):
     @abstractmethod
     def __init__(self, x, y, color):
         assert x in [1, 2, 3, 4, 5, 6, 7, 8] and y in [1, 2, 3, 4, 5, 6, 7, 8] and (
-                    color == "white" or color == "black")
+                color == "white" or color == "black")
         self._x = x
         self._y = y
         self._color = color
@@ -22,8 +22,12 @@ class Figure(ABC):
     def getCoordinates(self):
         return (self._x, self._y)
 
+    def getColor(self):
+        return self._color
+
     def __repr__(self):
         return str((self._x, self._y)) + ", " + self._color
+
 
 class King(Figure):
     def __init__(self, x, y, color):
@@ -32,7 +36,7 @@ class King(Figure):
     # can do the following moves
     def move(self, x, y, chessboard):
         # king can't move more than one step in x or y direction
-        if abs(self._x - x) <= 1 and abs(self._y - y) <= 1: # both differences 1 -> diagonal step
+        if abs(self._x - x) <= 1 and abs(self._y - y) <= 1:  # both differences 1 -> diagonal step
             super().move(x, y, chessboard)
 
     def __repr__(self):
@@ -47,23 +51,48 @@ class Farmer(Figure):
     # evaluates if a move is possible and then calls the super function to do the move
     def move(self, x, y, chessboard):
         if self._color == "white":
-            if self._x == x and self._y == 7 and y == 5 and isinstance(
-                    chessboard[(x, 6)], EmptyFigure) and isinstance(
-                chessboard[(x, 5)], EmptyFigure):
+            # move two steps from the starting position
+            if self._x == x and self._y == 7 and y == 5 and not (x, 6) in chessboard and not (x, 5) in chessboard:
                 super().move(x, y, chessboard)
+                return
+            # move to last field and change figure
+            if self._x == x and self._y == 1 and not (x, 1) in chessboard:
+                raise NotImplementedError
+            # move one step forward
+            if self._x == x and self._y - y == 1 and not (x, y) in chessboard:
+                super().move(x, y, chessboard)
+                return
+            # take enemy figure
+            if abs(self._x - x) == 1 and self._y - y == 1 and (x, y) in chessboard:
+                if chessboard[(x, y)].getColor() != self._color:
+                    super().move(x, y, chessboard)
+                    return
+        if self._color == "black":
+            # move two steps from the starting position
+            if self._x == x and self._y == 2 and y == 4 and not (x, 4) in chessboard and not (x, 3) in chessboard:
+                super().move(x, y, chessboard)
+                return
+            # move to last field and change figure
+            if self._x == x and self._y == 8 and not (x, 8) in chessboard:
+                raise NotImplementedError
+            # move one step forward
+            if self._x == x and y - self._y == 1 and not (x, y) in chessboard:
+                super().move(x, y, chessboard)
+                return
+            # take enemy figure
+            if abs(self._x - x) == 1 and self._y - y == -1 and (x, y) in chessboard:
+                if chessboard[(x, y)].getColor() != self._color:
+                    super().move(x, y, chessboard)
+                    return
+            raise NotImplementedError
 
     def __repr__(self):
         return "farmer, " + super().__repr__()
 
 
-class EmptyFigure:
-    def __repr__(self):
-        return "empty Field"
-
-
 chessboard = {}
-chessboard[(1, 7)] = Farmer(1, 9, "white")
+chessboard[(2, 6)] = Farmer(2, 6, "black")
+chessboard[(1, 7)] = Farmer(1, 7, "white")
 chessboard[(1, 6)] = EmptyFigure()
 chessboard[(1, 5)] = EmptyFigure()
-chessboard[(1, 7)].move(1, 5, chessboard)
-print(chessboard[(1, 5)])
+chessboard[(1, 7)].move(3, 6, chessboard)
