f03e7019b2bd65068cd6592d0c7958791e712d8f
Speedphil544 <45098581+Speedphil544@users.noreply.github.com>
2019-05-04 01:11:51 +0200

    changed chessboard to static variable

--- venv/Figures.py
+++ venv/Figures.py
@@ -10,21 +10,24 @@ In this module, we have all the figures
 
 # represents a figure on the board
 class Figure(ABC):
+    chessboard = {}
+
     @abstractmethod
     def __init__(self, x, y, color):
         assert x in [1, 2, 3, 4, 5, 6, 7, 8] and y in [1, 2, 3, 4, 5, 6, 7, 8] and (
-                color == "white" or color == "black")
+                color == "white" or color == "black") and not (x, y) in Figure.chessboard
         self._x = x
         self._y = y
         self._color = color
+        Figure.chessboard[(x, y)] = self
 
     # this method is called by the overwritten method in the inheriting class
-    def move(self, x, y, chessboard):
-        chessboard.pop(self._x, self._y)
+    def move(self, x, y):
+        Figure.chessboard.pop(self._x, self._y)
         self._x = x
         self._y = y
         # alte != neue Position
-        chessboard[(x, y)] = self
+        Figure.chessboard[(x, y)] = self
 
     def getCoordinates(self):
         return (self._x, self._y)
@@ -38,62 +41,64 @@ class Figure(ABC):
 
 class King(Figure):
     def __init__(self, x, y, color):
-        super().__init__(x, y, color)
+        Figure.__init__(self, x, y, color)
 
     # can do the following moves
-    def move(self, x, y, chessboard):
+    def move(self, x, y):
         # king can't move more than one step in x or y direction
         if abs(self._x - x) <= 1 and abs(self._y - y) <= 1:  # both differences 1 -> diagonal step
-            super().move(x, y, chessboard)
+            Figure.move(self, x, y)
 
     def __repr__(self):
-        return "king, " + super().__repr__()
+        return "king, " + Figure.__repr__(self)
 
 
 class Farmer(Figure):
     def __init__(self, x, y, color):
-        super().__init__(x, y, color)
+        Figure.__init__(self, x, y, color)
 
     # evaluates if a move is possible and then calls the super function to do the move
-    def move(self, x, y, chessboard):
+    def move(self, x, y):
         if self._color == "white":
             # move two steps from the starting position
-            if self._x == x and self._y == 7 and y == 5 and not (x, 6) in chessboard and not (x, 5) in chessboard:
-                super().move(x, y, chessboard)
+            if self._x == x and self._y == 7 and y == 5 and not (x, 6) in Figure.Figure.chessboard and not (x,
+                                                                                                            5) in Figure.Figure.chessboard:
+                Figure.move(self, x, y)
                 return
             # move to last field and change figure
-            if self._x == x and self._y == 1 and not (x, 1) in chessboard:
+            if self._x == x and self._y == 1 and not (x, 1) in Figure.chessboard:
                 raise NotImplementedError
             # move one step forward
-            if self._x == x and self._y - y == 1 and not (x, y) in chessboard:
-                super().move(x, y, chessboard)
+            if self._x == x and self._y - y == 1 and not (x, y) in Figure.chessboard:
+                Figure.move(self, x, y)
                 return
             # take enemy figure
-            if abs(self._x - x) == 1 and self._y - y == 1 and (x, y) in chessboard:
-                if chessboard[(x, y)].getColor() != self._color:
-                    super().move(x, y, chessboard)
+            if abs(self._x - x) == 1 and self._y - y == 1 and (x, y) in Figure.chessboard:
+                if Figure.chessboard[(x, y)].getColor() != self._color:
+                    Figure.move(self, x, y)
                     return
         if self._color == "black":
             # move two steps from the starting position
-            if self._x == x and self._y == 2 and y == 4 and not (x, 4) in chessboard and not (x, 3) in chessboard:
-                super().move(x, y, chessboard)
+            if self._x == x and self._y == 2 and y == 4 and not (x, 4) in Figure.chessboard and not (x,
+                                                                                                     3) in Figure.chessboard:
+                Figure.move(self, x, y)
                 return
             # move to last field and change figure
-            if self._x == x and self._y == 8 and not (x, 8) in chessboard:
+            if self._x == x and self._y == 8 and not (x, 8) in Figure.chessboard:
                 raise NotImplementedError
             # move one step forward
-            if self._x == x and y - self._y == 1 and not (x, y) in chessboard:
-                super().move(x, y, chessboard)
+            if self._x == x and y - self._y == 1 and not (x, y) in Figure.chessboard:
+                Figure.move(self, x, y)
                 return
             # take enemy figure
-            if abs(self._x - x) == 1 and self._y - y == -1 and (x, y) in chessboard:
-                if chessboard[(x, y)].getColor() != self._color:
-                    super().move(x, y, chessboard)
+            if abs(self._x - x) == 1 and self._y - y == -1 and (x, y) in Figure.chessboard:
+                if Figure.chessboard[(x, y)].getColor() != self._color:
+                    Figure.move(self, x, y)
                     return
             raise NotImplementedError
 
     def __repr__(self):
-        return "farmer, " + super().__repr__()
+        return "farmer, " + Figure.__repr__(self)
 
 
 class Tower(Figure):
@@ -101,19 +106,19 @@ class Tower(Figure):
         Figure.__init__(self, x, y, color)
 
     # evaluates if a move is possible and then calls the super function to do the move
-    def move(self, x, y, chessboard):
-        if (x, y) in chessboard:
-            if chessboard[(x, y)].getColor() == self._color:
+    def move(self, x, y):
+        if (x, y) in Figure.chessboard:
+            if Figure.chessboard[(x, y)].getColor() == self._color:
                 raise NotImplementedError
         if self._y == y:
-            if not list(filter(lambda xIt: (xIt, y) in chessboard,
+            if not list(filter(lambda xIt: (xIt, y) in Figure.chessboard,
                                range(self._x + np.sign(x - self._x), x, np.sign(x - self._x)))):
-                super().mover(x, y, chessboard)
+                Figure.move(self, x, y)
                 return
         if self._x == x:
-            if not list(filter(lambda yIt: (x, yIt) in chessboard,
+            if not list(filter(lambda yIt: (x, yIt) in Figure.chessboard,
                                range(self._y + np.sign(y - self._y), y, np.sign(y - self._y)))):
-                Figure.move(self, x, y, chessboard)
+                Figure.move(self, x, y)
                 return
         raise NotImplementedError
 
@@ -123,30 +128,30 @@ class Knight(Figure):
         Figure.__init__(self, x, y, color)
 
     # evaluates if a move is possible and then calls the super function to do the move
-    def move(self, x, y, chessboard):
-        if (x, y) in chessboard:
-            if chessboard[(x, y)].getColor() == self._color:
+    def move(self, x, y):
+        if (x, y) in Figure.chessboard:
+            if Figure.chessboard[(x, y)].getColor() == self._color:
                 raise NotImplementedError
         if (abs(self._x - x) == 2 and abs(self._y - y) == 1) or (abs(self._y - y) == 2 and abs(self._x - x) == 1):
-            Figure.move(self, x, y, chessboard)
+            Figure.move(self, x, y)
             return
         raise NotImplementedError
 
 
 class Bishop(Figure):
     def __init__(self, x, y, color):
-        super().__init__(x, y, color)
+        Figure.__init__(self, x, y, color)
 
     # evaluates if a move is possible and then calls the super function to do the move
-    def move(self, x, y, chessboard):
-        if (x, y) in chessboard:
-            if chessboard[(x, y)].getColor() == self._color:
+    def move(self, x, y):
+        if (x, y) in Figure.chessboard:
+            if Figure.chessboard[(x, y)].getColor() == self._color:
                 raise NotImplementedError
         if abs(self._x - x) == abs(self._y - y):
-            if not list(filter(lambda it: it in chessboard,
+            if not list(filter(lambda it: it in Figure.chessboard,
                                zip(range(self._x + np.sign(x - self._x), x, np.sign(x - self._x)),
                                    range(self._y + np.sign(y - self._y), y, np.sign(y - self._y))))):
-                Figure.move(self, x, y, chessboard)
+                Figure.move(self, x, y)
                 return
         raise NotImplementedError
 
@@ -155,21 +160,19 @@ class Queen(Bishop, Tower):
     def __init__(self, x, y, color):
         Figure.__init__(self, x, y, color)
 
-    def move(self, x, y, chessboard):
+    def move(self, x, y):
         try:
-            Bishop.move(self, x, y, chessboard)
+            Bishop.move(self, x, y)
             return
         except:
             NotImplementedError
         try:
-            Tower.move(self, x, y, chessboard)
+            Tower.move(self, x, y)
             return
         except:
             NotImplementedError
         raise NotImplementedError
 
 
-chessboard = {}
-chessboard[(3, 6)] = Farmer(1, 6, "black")
-chessboard[(2, 7)] = Queen(2, 7, "white")
-chessboard[(2, 7)].move(7, 5, chessboard)
+horse = Knight(1, 1, "white")
+print(Figure.chessboard)
